83ae332358bd3eebe58026efb064ff36c7141f29
zandrij <ajosuesh@gmail.com>
2023-02-05 01:14:14 -0500

    update validationtoken

--- src/controllers/utils/utils.js
+++ src/controllers/utils/utils.js
@@ -12,6 +12,39 @@ const validSession = (req, res) => {
     }
 }
 
+const verifyToken = (req, res, next) => {
+  const token = req.headers["snake-x"];
+  //verificamos el token
+  if (!token) {
+    return res.status(401).send("Access denied. No token provided.");
+  }
+
+  try {
+      //verificar si el token es valido
+      const decoded = jwt.verify(token, process.env.SECRET);
+      req.user = decoded;
+
+      UsersJwt.findOne({
+        where: { user_id: decoded.id}
+      })
+      .then(res => {
+        bcrypt.compare(token, res.token_encrypt, async (err, result) => {
+          if(err) return res.status(400).json({ errors: errors.array() });
+
+          if(result){
+            next();
+          }else{
+            res.status(400).send("Invalid token.");
+          }
+        });
+      })
+      next();
+  } catch (error) {
+      res.status(400).send("Invalid token.");
+  }
+}
+
 module.exports = {
-    validSession
+    validSession,
+    verifyToken
 }
--- src/router/router.js
+++ src/router/router.js
@@ -1,47 +1,13 @@
 const express = require("express");
 const { check } = require("express-validator");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const {
   storeUser,
   loginUser,
   getUser,
 } = require("../controllers/users/users-controller");
-const UsersJwt = require("../models/users-jwt");
+const { verifyToken } = require("../controllers/utils/utils");
 
 const router = express.Router();
-
-const verifyToken = (req, res, next) => {
-  const token = req.headers["snake-x"];
-  //verificamos el token
-  if (!token) {
-    return res.status(401).send("Access denied. No token provided.");
-  }
-
-  try {
-      //verificar si el token es valido
-      const decoded = jwt.verify(token, process.env.SECRET);
-      req.user = decoded;
-
-      UsersJwt.findOne({
-        where: { user_id: decoded.id}
-      })
-      .then(res => {
-        bcrypt.compare(token, res.token_encrypt, async (err, result) => {
-          if(err) return res.status(400).json({ errors: errors.array() });
-
-          if(result){
-            next();
-          }else{
-            res.status(400).send("Invalid token.");
-          }
-        });
-      })
-      next();
-  } catch (error) {
-      res.status(400).send("Invalid token.");
-  }
-}
 
 router.post("/register", [
   check('email').isEmail(),
